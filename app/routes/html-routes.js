92a8fd2247ed927304365a9da05a0b17e1009d6c
Joseph Huynh <joe.huynh@gmail.com>
2018-05-14 17:43:13 -0700

    Initial commit

---
+++ app/routes/html-routes.js
@@ -0,0 +1,19 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+
+    // add code to send the main.html file
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/main.html"));
+    });
+};
